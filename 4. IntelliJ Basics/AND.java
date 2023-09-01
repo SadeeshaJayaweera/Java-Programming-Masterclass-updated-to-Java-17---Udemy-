408f94e40c5b6e101e14fad731c2fb4fcd4433b7
Sadeesha_Jayaweera <132116738+SadeeshaJayaweera@users.noreply.github.com>
2023-09-01 11:08:16 +0530

    Add files via upload

---
+++ 4. IntelliJ Basics/AND.java
@@ -0,0 +1,17 @@
+public class AND {
+    public static void main(String[] args)
+    {
+      int highestScore = 100;
+      int academicScore=100;
+
+      if(highestScore==100 && academicScore==100)
+      {
+          System.out.printf("Congratulations! You are the Highest Scorer");
+      }
+
+      if (highestScore!=100 && academicScore!=100)
+      {
+          System.out.printf("Sorry, You are not the Highest Scorer");
+      }
+    }
+}
---
+++ 4. IntelliJ Basics/IF.java
@@ -0,0 +1,17 @@
+public class IF {
+    public static void main( String []args)
+    {
+        String Alien ="false";
+        if(Alien=="false")
+        {
+            System.out.println("It is not an Alien");
+            System.out.println("That is a Good  News");
+        }
+
+        else
+        {
+            System.out.println("It is an Alien " );
+            System.out.println("Damn, i'm Scared of aliens ");
+        }
+    }
+}
